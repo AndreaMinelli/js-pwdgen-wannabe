fc3d8dddb60462d382a5a5aa3fdd55b647e5fc8d
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-02 15:05:48 +0100

    Collegata variabile a elemento target

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+ Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 
+*/
+
+//Inserisco l'elemento target in una variabile
+
+const password = document.getElementById('generated-password');
+console.log(password)
